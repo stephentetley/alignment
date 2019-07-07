29bf60797bd68fee7294264f0880be58315fc977
stephentetley <stephen.tetley@gmail.com>
2019-07-07 14:54:48 +0100

    Lookup at zippers as a way to process tree differences and patches.

---
+++ test/Temp01.fsx
@@ -0,0 +1,112 @@
+﻿#r "netstandard"
+
+/// Oleg Kiselyov "The simplest way to turn a tree or any other enumerable data structure into a stream is through a zipper."
+
+
+type Tree23 = 
+    | Leaf
+    | Node2 of Tree23 * Tree23
+    | Node3 of Tree23 * Tree23 * Tree23
+
+
+type MetaVar = int
+
+type Tree23C<'phi> = 
+    | Hole of 'phi
+    | LeafC
+    | Node2C of Tree23C<'phi> * Tree23C<'phi>
+    | Node3C of Tree23C<'phi> * Tree23C<'phi> * Tree23C<'phi>
+
+type Change23<'phi> = (Tree23C<'phi> * Tree23C<'phi>)
+
+let del (context : Tree23C<MetaVar>) (tree : Tree23) : Map<MetaVar, Tree23> option = 
+    let rec work ctx t1 acc fk sk  = 
+        match (ctx,t1) with
+        | LeafC, Leaf -> sk acc
+        | Node2C(x,y), Node2(a,b) -> 
+            work x a acc fk (fun ac1 -> 
+            work y b ac1 fk (fun ac2 -> 
+            sk ac2))
+        | Node3C(x,y,z), Node3(a,b,c) -> 
+            work x a acc fk (fun ac1 -> 
+            work y b ac1 fk (fun ac2 -> 
+            work z c ac2 fk (fun ac3 -> 
+            sk ac3)))
+        | Hole(ix), t ->
+            match Map.tryFind ix acc with
+            | None -> sk (Map.add ix t acc)
+            | Some t1 -> if t = t1 then sk acc else fk ()
+        | _, _ -> fk ()
+    work context tree Map.empty (fun _ -> None) (fun x -> Some x)
+
+let ins (context : Tree23C<MetaVar>) (lookups : Map<MetaVar, Tree23>) : Tree23 option = 
+    let rec work ctx fk sk = 
+        match ctx with
+        | LeafC -> sk Leaf
+        | Node2C(x,y) -> 
+            work x fk (fun v1 ->
+            work y fk (fun v2 ->
+            sk (Node2(v1,v2))))
+        | Node3C(x,y,z) -> 
+            work x fk (fun v1 ->
+            work y fk (fun v2 ->
+            work z fk (fun v3 ->
+            sk (Node3(v1,v2,v3)))))
+        | Hole ix -> 
+            match Map.tryFind ix lookups with
+            | None -> fk ()
+            | Some t -> sk t
+    work context (fun _ -> None) (fun x -> Some x)
+
+let applyChange (change: Change23<MetaVar>) (original : Tree23) : Tree23 option = 
+    match change with | (d,i) -> Option.bind (fun tree -> ins i tree) (del d original) 
+
+
+let extract (operation : Tree23 -> MetaVar option) (tree : Tree23) : Tree23C<MetaVar> = 
+    let rec workP t1 cont = 
+        match t1 with
+        | Leaf -> cont LeafC
+        | Node2(a,b) -> 
+            workE a (fun v1 ->
+            workE b (fun v2 -> 
+            cont (Node2C(v1,v2))))
+        | Node3(a,b,c) -> 
+            workE a (fun v1 ->
+            workE b (fun v2 -> 
+            workE c (fun v3 -> 
+            cont (Node3C(v1,v2,v3)))))
+    and workE t1 cont = 
+        match operation t1 with
+        | None -> workP tree cont
+        | Some ix -> cont (Hole ix)
+    workE tree (fun x -> x)
+
+/// Not implemeneted as the paper says its wrong anyway
+let ics (tree1 : Tree23) (tree2 : Tree23) : Tree23 -> MetaVar option = 
+    fun _ -> None
+
+let faultyChangeTree23 (tree1 : Tree23) (tree2 : Tree23) : Change23<MetaVar> = 
+    (extract (ics tree1 tree2) tree1, extract (ics tree1 tree2) tree2)
+
+type Patch23 = Tree23C<MetaVar>
+
+let gcp (context1 : Tree23C<'var>) (context2 : Tree23C<'var>) : Tree23C<Change23<'var>> = 
+    let rec work ctx1 ctx2 cont = 
+        match ctx1, ctx2 with
+        | LeafC, LeafC -> cont LeafC
+        | Node2C(a,b), Node2C(x,y) -> 
+            work a x (fun v1 -> 
+            work b y (fun v2 -> 
+            cont (Node2C(v1,v2))))
+        | Node3C(a,b,c), Node3C(x,y,z) -> 
+            work a x (fun v1 -> 
+            work b y (fun v2 -> 
+            work c z (fun v3 -> 
+            cont (Node3C(v1,v2,v3)))))
+        | a, b -> 
+            cont (Hole(a,b))
+    work context1 context2 (fun x -> x)
+
+
+
+
---
+++ test/Zipper1.fsx
@@ -0,0 +1,93 @@
+﻿#r "netstandard"
+
+#I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190616\lib\netstandard2.0"
+#r "SLFormat.dll"
+
+
+open SLFormat.RoseTree
+
+let tree1 : RoseTree<string> = 
+    let leaf s = RoseTree(s,[])
+    RoseTree("+", [ RoseTree("*", [leaf "a"; leaf "b"]); RoseTree("/", [leaf "c"; leaf "d"]) ])
+
+let demo01 () = 
+    drawTree tree1 |> printfn "%s"
+
+type Path<'a> = 
+    | Root
+    | Path of label : 'a * lefts : RoseTree<'a> list * ups : Path<'a> * rights : RoseTree<'a> list
+
+
+type Location<'a> =
+    | Location of RoseTree<'a> * Path<'a>
+
+let treeRoot (source : RoseTree<'a>) : Location<'a> = 
+    Location(source, Root)
+
+let change (newValue : 'a) (loc : Location<'a>) : Location<'a> =
+    match loc with | Location(RoseTree(_,kids), path) -> Location(RoseTree(newValue,kids), path)
+
+let goLeft (loc : Location<'a>) : Location<'a> option = 
+    match loc with
+    | Location(t1, Path(a, l1 :: lefts, ups, rights)) -> Location(l1, Path(a, lefts, ups, t1 :: rights)) |> Some
+    | Location(_, Path(_, [], _, _))  -> None
+    | Location(_, Root) -> None
+
+let goRight (loc : Location<'a>) : Location<'a> option = 
+    match loc with
+    | Location(t1, Path(a, lefts, ups, r1 :: rights)) -> Location(r1, Path(a, t1 :: lefts, ups, rights)) |> Some
+    | Location(_, Path(_, _, _, []))  -> None
+    | Location(_, Root) -> None
+
+let goUp (loc : Location<'a>) : Location<'a> option = 
+    match loc with
+    | Location(t1, Path(a, lefts, ups, rights)) -> 
+        let kids = List.rev lefts @ (t1 :: rights) in Location(RoseTree(a, kids), ups) |> Some
+    | Location(_, Root) -> None
+
+
+    
+let goDown (loc : Location<'a>) : Location<'a> option = 
+    match loc with
+    | Location(RoseTree(a, x :: xs), path) -> Location(x, Path(a, [], path, xs)) |> Some
+    | Location(RoseTree(_ , []), _) -> None
+
+
+let demo02 () = 
+    treeRoot tree1
+
+
+let unwind (loc : Location<'a>) : RoseTree<'a> = 
+    let rec work (ctx:Path<'a>) t1 cont = 
+        match ctx with
+        | Root -> cont t1
+        | Path(a,lefts,ups,rights) -> 
+            let t2 = RoseTree(a,lefts @ t1 :: rights)
+            work ups t2 cont
+ 
+    match loc with | Location(tree1, path) -> work path tree1 (fun x -> x)
+
+
+let demo03 () = 
+    unwind <| treeRoot tree1
+
+let det o = match o with | None -> failwith "None" | Some (x) -> x
+
+let demo04 () = 
+    treeRoot tree1 |> (det << goDown) |> (det << goRight) |> change "/" |> unwind
+
+let demo05 () = 
+    treeRoot tree1 
+        |> (det << goDown) 
+        |> (det << goRight)
+        |> (det << goDown) 
+        |> change "1" 
+        |> (det << goRight) 
+        |> (change "2")
+        |> unwind
+
+
+
+
+
+
