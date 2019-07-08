c2cd96ddb8f6567f190d129825b5c258b6404165
stephentetley <stephen.tetley@gmail.com>
2019-07-08 08:52:42 +0100

    Moved tree and list diff code out of prototype scripts into library modules. At some point they will have to be optimized.

--- test/PatchingLists1.fsx
+++ test/PatchingLists1.fsx
@@ -1,83 +1,16 @@
 ﻿#r "netstandard"
 
 // From "Type safe diff for families of datatypes"
-// Eelco Lempsink, Sean Leather, Andres Lo\eh
+// Eelco Lempsink, Sean Leather, Andres Löh
 
 
-type Diff<'a> = 
-    | Ins of 'a
-    | Del of 'a
-    | Cpy of 'a
-   
-let insertCps (x:'a)  (items : 'a list) 
-                (_ : unit -> ('a list) option) 
-                (sk : 'a list -> ('a list) option) : ('a list) option = 
-    sk (x :: items)
-    
-
-    
-let deleteCps (x:'a)  (items : 'a list) 
-                (fk : unit -> ('a list) option) 
-                (sk : 'a list -> ('a list) option) : ('a list) option = 
-    match items with
-    | [] -> fk ()
-    | y :: ys -> if x = y then sk ys else fk ()
-
-
-let patch (patchSteps : Diff<'a> list) (source : 'a list) : ('a list) option = 
-    let rec work ps xs fk sk = 
-        match ps, xs with
-        | Ins(x) :: ds, ys -> 
-            work ds ys fk (fun ac ->
-            insertCps x ac fk sk)
-
-        | Del(x) :: ds, ys -> 
-            deleteCps x ys fk (fun ac ->
-            work ds ac fk sk)
-
-        | Cpy(x) :: ds, ys ->
-            deleteCps x ys fk (fun ac1 ->
-            work ds ac1 fk (fun ac2 -> 
-            insertCps x ac2 fk sk))
-
-        | [], [] -> sk []
-
-        | [], _ -> fk ()
-    work patchSteps source (fun _ -> None) (fun xs -> Some xs)
-
+#load "..\src\SLAlignment\ListDiff.fs"
+open SLAlignment.ListDiff
 
 let demo01 () = 
     patch [Cpy(1);Cpy(2);Ins(3);Cpy(4)] [1;2;4]
 
 
-let cost (diff: Diff<'a> list) : int = diff.Length
-
-let choose (dx : Diff<'a> list) (dy : Diff<'a> list) : Diff<'a> list = 
-    if cost dx <= cost dy then dx else dy
-
-let diff (list1 : 'a list) (list2 : 'a list) : Diff<'a> list = 
-    let rec work ks ls cont = 
-        match ks,ls with
-        | [], [] -> cont []
-        | [], y :: ys -> 
-            work [] ys (fun ac ->
-            cont (Ins(y) :: ac))
-        | x :: xs, [] -> 
-            work xs [] (fun ac ->
-            cont (Del(x) :: ac))
-        | x :: xs, y :: ys ->
-            if x = y then best3 x y xs ys cont else best2 x y xs ys cont
-    and best2 x y xs ys cont =
-        work xs (y::ys) (fun acD ->
-        work (x::xs) ys (fun acI -> 
-        cont (choose (Del(x) :: acD) (Ins(y) :: acI))))
-
-    and best3 x y xs ys cont =
-        work xs ys (fun acC ->
-        best2 x y xs ys (fun acB2 ->
-        cont (choose (Cpy(x) :: acC) acB2)))
-
-    work list1 list2 (fun xs -> xs)
 
 let demo02 () = diff [1;2;3;4;5] [1;3;4;6]
 
--- test/PatchingTrees1.fsx
+++ test/PatchingTrees1.fsx
@@ -1,98 +1,17 @@
 ﻿#r "netstandard"
 
 // From "Type safe diff for families of datatypes"
-// Eelco Lempsink, Sean Leather, Andres Lo\eh
+// Eelco Lempsink, Sean Leather, Andres Löh
 
-type Tree<'a> = 
-    | Node of label : 'a * kids : Tree<'a> list
-
-
-
-type Diff<'a> = 
-    | Ins of 'a * arity : int
-    | Del of 'a * arity : int
-    | Cpy of 'a * arity : int
-   
-let insertCps (x:'a) 
-              (arity:int) 
-              (trees : Tree<'a> list) 
-              (fk : unit -> (Tree<'a> list) option)
-              (sk : Tree<'a> list -> (Tree<'a> list) option) : (Tree<'a> list) option = 
-    try 
-        let (ys, yss) = List.splitAt arity trees
-        sk (Node(x,ys) :: yss)
-    with
-    | _ -> fk ()
-
-    
-let deleteCps (x:'a) 
-              (arity:int) 
-              (trees : Tree<'a> list)
-              (fk : unit -> (Tree<'a> list) option)
-              (sk : Tree<'a> list -> (Tree<'a> list) option) : (Tree<'a> list) option = 
-    match trees with
-    | [] -> fk ()
-    | Node(y, ys) :: yss -> 
-        if x = y && arity = ys.Length then sk (ys @ yss) else fk ()
+#load "..\src\SLAlignment\TreeDiff.fs"
+open SLAlignment.TreeDiff
 
 
-let patch (patchSteps : Diff<'a> list) (source : Tree<'a> list) : (Tree<'a> list) option = 
-    let rec work ps xs fk sk = 
-        match ps, xs with
-        | Ins(x, sz) :: ds, ys -> 
-            work ds ys fk (fun ac ->
-            insertCps x sz ac fk sk)
 
-        | Del(x, sz) :: ds, ys -> 
-            deleteCps x sz ys fk (fun ac ->
-            work ds ac fk sk)
+let demo01 () = 
+    patch   [ Cpy(1,0); Cpy(2,0); Ins(3,0); Cpy(4,0) ] 
+            [ Node(1,[]); Node(2,[]); Node(4,[]) ]
 
-        | Cpy(x, sz) :: ds, ys ->
-            deleteCps x sz ys fk (fun ac1 ->
-            work ds ac1 fk (fun ac2 -> 
-            insertCps x sz ac2 fk sk))
-
-        | [], [] -> sk []
-
-        | [], _ -> fk ()
-    work patchSteps source (fun _ -> None) (fun xs -> Some xs)
-
-
-//let demo01 () = 
-//    patch [Cpy(1);Cpy(2);Ins(3);Cpy(4)] [1;2;4]
-
-
-let cost (diff: Diff<'a> list) : int = diff.Length
-
-let choose (dx : Diff<'a> list) (dy : Diff<'a> list) : Diff<'a> list = 
-    if cost dx <= cost dy then dx else dy
-
-let diff (list1 : Tree<'a> list) (list2 : Tree<'a> list) : Diff<'a> list = 
-    let rec work ks ls cont = 
-        match ks,ls with
-        | [], [] -> cont []
-        | [], (Node(y,ys) :: yss) -> 
-            work [] (ys @ yss) (fun ac ->
-            cont (Ins(y, ys.Length) :: ac))
-
-        | (Node(x,xs) :: xss), [] -> 
-            work (xs @xss) [] (fun ac ->
-            cont (Del(x, xs.Length) :: ac))
-
-        | (Node(x,xs) :: xss), (Node(y,ys) :: yss) ->
-            if x = y && xs.Length = ys.Length then best3 x xs xss y ys yss cont else best2 x xs xss y ys yss cont
-
-    and best2 x xs xss y ys yss cont =
-        work (xs @ xss) (Node(y,ys) :: yss) (fun acD ->
-        work (Node(x,xs) :: xss) (ys @ yss) (fun acI -> 
-        cont (choose (Del(x, xs.Length) :: acD) (Ins(y, ys.Length) :: acI))))
-
-    and best3 x xs xss y ys yss cont =
-        work xs ys (fun acC ->
-        best2 x xs xss y ys yss (fun acB2 ->
-        cont (choose (Cpy(x, xs.Length) :: acC) acB2)))
-
-    work list1 list2 (fun xs -> xs)
 
 let demo02 () = diff [ Node(1, [Node(2,[]); Node(3,[]); Node(4,[]); Node(5,[])]) ] 
                      [ Node(1, [Node(3,[]); Node(4,[]) ; Node(6,[])]) ]
