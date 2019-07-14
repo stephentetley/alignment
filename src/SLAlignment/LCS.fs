b9de4381e951de1e20c8bd0c3c09bda24a777207
stephentetley <stephen.tetley@gmail.com>
2019-07-14 12:02:22 +0100

    Corrected indexing bug in longest common subsequence. Myers's paper uses 1-indexing for strings, Fsharp uses 0-indexing of course.

--- src/SLAlignment/LCS.fs
+++ src/SLAlignment/LCS.fs
@@ -17,17 +17,26 @@ module LCS =
         let n = source.Length
         let mAX = m + n
         let size = mAX * 2 + 1      
-        let v : int [] = Array.zeroCreate mAX
-        let lookupV (ix: int) = v.[ix + mAX] 
-        let updateV (ix: int) (value: int) = v.[ix+ mAX] <- value
+        printfn "Array size=%i" size
+        let v : int [] = Array.zeroCreate size
+        let lookupV (ix: int) = 
+            let idx = ix + mAX 
+            printfn "lookupV: %i => %i (size: %i)" ix idx v.Length 
+            v.[idx] 
+        let updateV (ix: int) (value: int) = 
+            let idx = ix + mAX
+            printfn "updateV: %i => %i (size: %i)" ix idx v.Length 
+            v.[idx] <- value
         let mutable sesLength : int = -1
         try
             for d in 0 .. mAX do
                 for k in -d .. 2 .. d do
+                    printfn "(d=%i, k=%i)" d k
                     let mutable (x : int) = 
                         if k = -d || k <> d && lookupV (k-1) < lookupV (k+1) then lookupV  (k+1) else lookupV (k-1) + 1
                     let mutable (y : int) = x - k
-                    while x < n && y < m && source.[x+1] = target.[y+1] do
+                    /// Note Myers's paper uses 1-indexing
+                    while x < n && y < m && source.[x] = target.[y] do
                         x <- x + 1
                         y <- y + 1
                     updateV k x
@@ -38,6 +47,7 @@ module LCS =
             None
         with
         | ExnBreak -> Some sesLength
+        | exn -> printfn "%s" exn.Message; None
 
 
 
--- test/TestLCS.fsx
+++ test/TestLCS.fsx
@@ -7,6 +7,14 @@ open SLAlignment.LCS
 let pathA = "abcabba"
 let pathB = "cbabac"
 
+
+let test0a () = 
+    let d = 11
+    for k in -d .. 2 .. d do 
+        printfn "%i" k
+
+
+
 let test01 () = 
     greedyLCS (Seq.toArray pathA) (Seq.toArray pathB)
 
